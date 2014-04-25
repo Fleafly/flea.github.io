0842a5ad3f61ce81a529461714c99f915a082321
Fleafly <vili78@gmail.com>
2014-04-25 21:05:32 +0300

    del index and file.write new

--- generate.rb
+++ generate.rb
@@ -1,4 +1,28 @@
-File.write("hi.txt",'Grand mama')
+File.write("./bg/index.html",'<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset = "utf-8">
+	<title> Моето местенце </title>
+	<link rel="stylesheet" type="text/css" href="../styles/style.css">	
+</head>
+<body>
+
+	<h1>Вили</h1>
+
+		<article class = "main" 
+			title = "Привет!" >
+			<h3>
+				<p id="Firstp">Скачам на кокили.</p>
+				<p id="Secondp">Харесвам да скачам на кокили.</p>	
+				<p id="Thirdp">Много харесвам да скачам на кокили.</p>
+			</h3>
+				
+		</article>
+		
+		<a href="../en/index.html">EN</a>
+</body>
+</html>')
 
 
 
