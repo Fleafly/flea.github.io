43afb1580f74d20fb32925864d17340360f96ab5
Fleafly <vili78@gmail.com>
2014-05-09 21:59:32 +0300

    del doctype cont and replace with file.write funct

--- generate.rb
+++ generate.rb
@@ -1,5 +1,6 @@
 #encoding: utf-8
- File.write("./bg/index.html",'<!DOCTYPE html>
+
+html_template='<!DOCTYPE html>
 
 <html>
 <head>
@@ -23,53 +24,18 @@
 		
 		<a href="../en/index.html">ENG</a>
 </body>
-</html>')
+</html>'
 
-#eng_text={
-# 	:myname=> "Vili",
-# 	:firstptext=> "Give it away",}
+en_translation={
+	:myname=> "Vili",
+	:firstptext=> "Give it away",}
 
-# bg_text={
-# 	:myname=> "Вили",
-# 	:firstptext=> "Скачам на кокили.",}
+bg_translation={
+	:myname=> "Вили",
+	:firstptext=> "Скачам на кокили.",}
 
-
-#encoding: utf-8
- File.write("./en/index.html",'<!DOCTYPE html>
-
-<html>
-<head>
-	<meta charset = "utf-8">
-	<title> Моето местенце </title>
-	<link rel="stylesheet" type="text/css" href="../styles/style.css">	
-</head>
-<body>
-
-	<h1>%{myname}</h1>
-
-		<article class = "main" 
-			title = "Привет!" >
-			<h3>
-				<p id="Firstp">%{firstptext}</p>
-				<p id="Secondp">Харесвам да скачам на кокили.</p>	
-				<p id="Thirdp">Много харесвам да скачам на кокили.</p>
-			</h3>
-				
-		</article>
-		
-		<a href="../bg/index.html">BG</a>
-</body>
-</html>')
-
-#eng_text={
-# 	:myname=> "Vili",
-# 	:firstptext=> "Give it away",}
-
-# bg_text={
-# 	:myname=> "Вили",
-# 	:firstptext=> "Скачам на кокили.",}
-
-
+File.write("./en/index.html", html_template % en_translation)
+File.write("./bg/index.html", html_template % bg_translation)
 
 
 
