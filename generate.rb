73e66598533d000d304f9466bcfc3a03aeb6a955
Fleafly <vili78@gmail.com>
2014-05-09 22:35:38 +0300

    extract cont to template.html

--- generate.rb
+++ generate.rb
@@ -1,31 +1,5 @@
 #encoding: utf-8
 
-html_template='<!DOCTYPE html>
-
-<html>
-<head>
-	<meta charset = "utf-8">
-	<title> Моето местенце </title>
-	<link rel="stylesheet" type="text/css" href="../styles/style.css">	
-</head>
-<body>
-
-	<h1>%{myname}</h1>
-
-		<article class = "main" 
-			title = "Привет!" >
-			<h3>
-				<p id="Firstp">%{firstptext}</p>
-				<p id="Secondp">Харесвам да скачам на кокили.</p>	
-				<p id="Thirdp">Много харесвам да скачам на кокили.</p>
-			</h3>
-				
-		</article>
-		
-		<a href="../en/index.html">ENG</a>
-</body>
-</html>'
-
 en_translation={
 	:myname=> "Vili",
 	:firstptext=> "Give it away",}
@@ -34,10 +8,12 @@ bg_translation={
 	:myname=> "Вили",
 	:firstptext=> "Скачам на кокили.",}
 
+html_template =File.read("./template.html").force_encoding("utf-8")
+
 File.write("./en/index.html", html_template % en_translation)
 File.write("./bg/index.html", html_template % bg_translation)
 
-
+puts (html_template.encoding)
 
 
 
