d71bb1fa0a353dfbe6626f5b1a4f5742f1adf84f
Fleafly <vili78@gmail.com>
2014-05-09 20:41:17 +0300

    generate both language versions

--- generate.rb
+++ generate.rb
@@ -21,7 +21,7 @@
 				
 		</article>
 		
-		<a href="../en/index.html">EN</a>
+		<a href="../en/index.html">ENG</a>
 </body>
 </html>')
 
@@ -32,6 +32,46 @@
 # bg_text={
 # 	:myname=> "Вили",
 # 	:firstptext=> "Скачам на кокили.",}
+
+
+#encoding: utf-8
+ File.write("./en/index.html",'<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset = "utf-8">
+	<title> Моето местенце </title>
+	<link rel="stylesheet" type="text/css" href="../styles/style.css">	
+</head>
+<body>
+
+	<h1>%{myname}</h1>
+
+		<article class = "main" 
+			title = "Привет!" >
+			<h3>
+				<p id="Firstp">%{firstptext}</p>
+				<p id="Secondp">Харесвам да скачам на кокили.</p>	
+				<p id="Thirdp">Много харесвам да скачам на кокили.</p>
+			</h3>
+				
+		</article>
+		
+		<a href="../bg/index.html">BG</a>
+</body>
+</html>')
+
+#eng_text={
+# 	:myname=> "Vili",
+# 	:firstptext=> "Give it away",}
+
+# bg_text={
+# 	:myname=> "Вили",
+# 	:firstptext=> "Скачам на кокили.",}
+
+
+
+
 
 
 
