e2f9b057ac3bc6fcb72f3dc6f935eddf0c6cf0ae
Fleafly <vili78@gmail.com>
2014-05-23 22:02:41 +0300

    eng and bg yaml instead of bg/en translation

--- generate.rb
+++ generate.rb
@@ -1,13 +1,17 @@
 #encoding: utf-8
+require 'yaml'
+
+en_translation=YAML.load_file("./eng_index.yml")
+bg_translation=YAML.load_file("./bg_index.yml")
 html_content = File.read("./template/template.html")
 
-en_translation={
-	:myname=> "Vili",
-	:firstptext=> "Give it away",}
+# en_translation={
+# 	:myname=> "Vili",
+# 	:firstptext=> "Give it away",}
 
-bg_translation={
-	:myname=> "Вили",
-	:firstptext=> "Скачам на кокили.",}
+# bg_translation={
+# 	:myname=> "Вили",
+# 	:firstptext=> "Скачам на кокили.",}
 
 html_template =File.read("./template/template.html").force_encoding("utf-8")
 
