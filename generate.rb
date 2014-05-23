8bcca4e80f0e0eefda5bfce2c801a3e8d5066358
Fleafly <vili78@gmail.com>
2014-05-23 20:29:52 +0300

    template file to template folder

--- generate.rb
+++ generate.rb
@@ -8,7 +8,7 @@ bg_translation={
 	:myname=> "Вили",
 	:firstptext=> "Скачам на кокили.",}
 
-html_template =File.read("./template.html").force_encoding("utf-8")
+html_template =File.read("./template/template.html").force_encoding("utf-8")
 
 File.write("./en/index.html", html_template % en_translation)
 File.write("./bg/index.html", html_template % bg_translation)
